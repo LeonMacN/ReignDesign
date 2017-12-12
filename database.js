d6a81717d5af64b87b5f93c796aaff86447e4c3c
Ricardo Colmenarez <LeonMacNichols@gmail.com>
2017-12-11 22:50:27 -0300

    Initial commit

---
+++ database.js
@@ -0,0 +1,62 @@
+var axios = require('axios');
+var _ = require('lodash');
+var mongoose = require("mongoose");
+var feedsSchema, feedModel;
+
+module.exports = {
+  init: function() {
+    mongoose.Promise = require("bluebird");
+    mongoose
+      .connect("mongodb://localhost:32768/reignDesign", {
+        useMongoClient: true
+      })
+      .then(
+        function(response) {
+          console.log("Mongo Connected!");
+        },
+        function(err) {
+          console.log("Error connecting mongo!");
+        }
+      );
+
+    feedsSchema = mongoose.Schema({
+      created_at: Date,
+      title: String,
+      url: String,
+      author: String,
+      story_id: Number,
+      story_title: String,
+      story_url: String
+    });
+
+    feedModel = mongoose.model("Feed", feedsSchema);
+  },
+  feedsSchema: function() {
+    return feedsSchema;
+  },
+  feedModel: function() {
+    return feedModel;
+  },
+  populateDB: function() {
+    axios.get('https://hn.algolia.com/api/v1/search_by_date?query=nodejs').then(function(response){
+      var feeds = response.data.hits;
+      _.each(feeds, function(feed){
+        var feedObj = {
+          created_at: feed.created_at,
+          title: feed.title,
+          url: feed.url,
+          author: feed.author,
+          story_id: feed.story_id,
+          story_title: feed.story_title,
+          story_url: feed.story_url,
+        };
+        feedModel.find({story_id: feed.story_id}).then(function(resp, err){
+          if(resp.length === 0){
+            var data = new feedModel(feedObj);
+            data.save();
+          }
+        });
+      });
+    });
+  }
+};
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', ['watch', 'sass']);
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.src('./node_modules/bootstrap-sass/assets/fonts/bootstrap/*')
+    .pipe(gulp.dest('./public/css/bootstrap/'));
+
+gulp.task('watch', function() {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
